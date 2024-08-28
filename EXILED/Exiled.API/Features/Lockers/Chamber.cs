03fbe42f3bed5f07dac308008c153b444e61161b
SrLicht <francoquiroz_13@live.com>
2024-08-28 15:36:44 -0300

    MORE
    * Added GetRandomSpawnPoint() in Chamber
    
    * Added AddItemToSpawn((ItemType itemType, int quantity = 1, bool spawnIfIsOpen = false)) in Chamber
    
    * Added IsOpen in chamber.

--- EXILED/Exiled.API/Features/Lockers/Chamber.cs
+++ EXILED/Exiled.API/Features/Lockers/Chamber.cs
@@ -11,6 +11,7 @@ namespace Exiled.API.Features.Lockers
     using System.Linq;
 
     using Exiled.API.Enums;
+    using Exiled.API.Extensions;
     using Exiled.API.Features.Pickups;
     using Exiled.API.Interfaces;
     using MapGeneration.Distributors;
@@ -146,6 +147,11 @@ namespace Exiled.API.Features.Lockers
         }
 
         /// <summary>
+        /// Gets a value indicating whether the chamber is currently open.
+        /// </summary>
+        public bool IsOpen => Base.IsOpen;
+
+        /// <summary>
         /// Gets the <see cref="Stopwatch"/> of current cooldown.
         /// </summary>
         /// <remarks>Used in <see cref="CanInteract"/> check.</remarks>
@@ -162,6 +168,51 @@ namespace Exiled.API.Features.Lockers
         /// <param name="type"><see cref="ItemType"/> from <see cref="AcceptableTypes"/>.</param>
         /// <param name="amount">Amount of items that should be spawned.</param>
         public void SpawnItem(ItemType type, int amount) => Base.SpawnItem(type, amount);
+
+        /// <summary>
+        /// Adds an item of the specified type to the chamber's spawn list.
+        /// If the chamber is open and <paramref name="spawnIfIsOpen"/> is set to <see langword="true"/>,
+        /// the item is spawned immediately at a random spawn point within the chamber.
+        /// </summary>
+        /// <param name="itemType">The type of item to add to the spawn list.</param>
+        /// <param name="quantity">The number of items to add. Defaults to 1.</param>
+        /// <param name="spawnIfIsOpen">
+        /// If <see langword="true"/> and the chamber is open, the item is immediately spawned at a random spawn point.
+        /// Otherwise, the item is added to the spawn list and will spawn when the chamber is opened.
+        /// </param>
+        public void AddItemToSpawn(ItemType itemType, int quantity = 1, bool spawnIfIsOpen = false)
+        {
+            for (int i = 0; i < quantity; i++)
+            {
+                Pickup pickup = Pickup.Create(itemType);
+
+                if (spawnIfIsOpen && IsOpen)
+                {
+                    pickup.Position = GetRandomSpawnPoint();
+                    pickup.Spawn();
+                    continue;
+                }
+
+                Base._toBeSpawned.Add(pickup.Base);
+            }
+        }
+
+        /// <summary>
+        /// Gets a random spawn point within the chamber.
+        /// If multiple spawn points are available and <see cref="UseMultipleSpawnpoints"/> is <see langword="true"/>,
+        /// a random spawn point is selected from the available points.
+        /// Otherwise, the default spawn point is used.
+        /// </summary>
+        /// <returns>A <see cref="Vector3"/> representing the position of the selected spawn point.</returns>
+        public Vector3 GetRandomSpawnPoint()
+        {
+            if (UseMultipleSpawnpoints && Spawnpoints.Any())
+            {
+                return Spawnpoints.GetRandomValue().position;
+            }
+
+            return Spawnpoint.position;
+        }
 
         /// <summary>
         /// Gets the chamber by its <see cref="LockerChamber"/>.
