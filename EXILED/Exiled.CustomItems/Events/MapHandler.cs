5fa8fc83c2197799b870b829ee57e047562410b8
SrLicht <francoquiroz_13@live.com>
2024-09-24 16:29:31 -0300

    Using WaitingForPlayers
    * Thanks Alex
    
    Co-Authored-By: Alex Rouse <alex@leronix.net>

--- EXILED/Exiled.CustomItems/Events/MapHandler.cs
+++ EXILED/Exiled.CustomItems/Events/MapHandler.cs
@@ -15,14 +15,11 @@ namespace Exiled.CustomItems.Events
     /// </summary>
     internal sealed class MapHandler
     {
-        /// <inheritdoc cref="Exiled.Events.Handlers.Map.Generated"/>
-        public void OnMapGenerated()
+        /// <inheritdoc cref="Exiled.Events.Handlers.Server.WaitingForPlayers"/>
+        public void OnWaitingForPlayers()
         {
-            Timing.CallDelayed(0.8f, () => // Delay its necessary for the spawnpoints of lockers and rooms to be generated.
-            {
-                foreach (CustomItem customItem in CustomItem.Registered)
-                    customItem?.SpawnAll();
-            });
+            foreach (CustomItem customItem in CustomItem.Registered)
+                customItem?.SpawnAll();
         }
     }
 }
