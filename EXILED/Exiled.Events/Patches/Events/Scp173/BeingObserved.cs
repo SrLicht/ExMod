7703deaebb34b9fc2147d7661c38f95fd06711fc
SrLicht <francoquiroz_13@live.com>
2024-11-11 23:13:09 -0300

    var user detected
    no no no, bad dev dont use var in a complex method like a transpiler + dont let me build the project in release.

--- EXILED/Exiled.Events/Patches/Events/Scp173/BeingObserved.cs
+++ EXILED/Exiled.Events/Patches/Events/Scp173/BeingObserved.cs
@@ -1,4 +1,4 @@
-﻿// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
 // <copyright file="BeingObserved.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
@@ -38,7 +38,7 @@ namespace Exiled.Events.Patches.Events.Scp173
             Label continueLabel = generator.DefineLabel();
 
             const int offset = -4;
-            var scp173ExecuteEvent = typeof(EventManager)
+            MethodInfo scp173ExecuteEvent = typeof(EventManager)
                 .GetMethods(BindingFlags.Public | BindingFlags.Static)
                 .FirstOrDefault(m => m.Name == nameof(EventManager.ExecuteEvent)
                                      && !m.IsGenericMethod);
